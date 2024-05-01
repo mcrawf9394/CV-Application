47e4cf65a75d2ac4be866a82df609af55110b094
mcrawf9394 <mjc9394@gmail.com>
2024-05-01 12:42:03 -0400

    updated the readme from the template to a simple readme for the current project

--- src/experience.jsx
+++ src/experience.jsx
@@ -1,5 +1,8 @@
 import { useState } from "react";
 function Experience () {
-
+    const [companyName, setCompanyName] = useState('Acme')
+    const [positionTitle, setPositionTitle] = useState('Explosives R&D Test Dummy')
+    const [jobDescription, setDescription] = useState('Standing in the blast radius of newly developed explosive devices')
+    
 }
 export default Experience
